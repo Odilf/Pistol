fc85adc15b06860471ae8e263ebb06601c1fe348
Odilf <odysseas.maheras@gmail.com>
2021-08-04 08:48:20 +0300

    moved to SvelteKit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<!-- <footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer> -->
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	// import Caca from '$lib/Caca.svelte';
+</script>
+
+<svelte:head>
+	<title>Odilf Timer</title>
+</svelte:head>
