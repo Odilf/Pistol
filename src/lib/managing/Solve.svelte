254f8ce22c55c9f3eb5071a9825ba7d9291d8a8b
Odilf <odysseas.maheras@gmail.com>
2021-08-05 13:00:59 +0300

    added solve component using types

---
+++ src/lib/managing/Solve.svelte
@@ -0,0 +1,7 @@
+<script lang='ts'>
+	import type { Solve } from '$lib/storage/time_db'
+
+	export let solve: Solve;
+</script>
+
+Id is {solve.id}, time is {solve.time.toFixed(2)}, date is {solve.date.getTime()}, scrmable is {solve.scramble}
