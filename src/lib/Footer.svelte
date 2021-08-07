3d3742a049a78eff87fd0a56d1f24b8704d9e929
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:38:59 +0300

    added footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { Footer, Button } from 'svelte-materialify'
+	const links = [
+		{name: 'Settings', url: '/settings'},
+		{name: 'About', url: '/about'},
+		{name: 'GitHub', url: 'https://github.com/Odilf/Odilf-Timer'},
+		]
+</script>
+
+<div class='center'>
+	<Footer class="center">
+			<div class="buttons">
+				<Button on:click={() => location.href = '/settings'}> Settings </Button>
+				<Button on:click={() => location.href = '/about'}> About </Button>
+				<Button on:click={() => window.open('https://github.com/Odilf/Odilf-Timer','_blank')}> <img src='static/github.svg' alt='GitHub'/> </Button>
+			</div>
+		<p class="center" style="width:100%">
+			2021 - <b>Odilf</b>
+		</p>
+	</Footer>
+</div>
+
+<style lang="scss">
+	p {
+		margin: 0.5em;
+	}
+	.center {
+		display: flex;
+		justify-content: center;
+
+	}
+
+	.buttons {
+		flex-grow: 1;
+
+		//Text centering
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
