aced86a89d80bbe8945dde8ca364f8ce0a3da5b1
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:40:15 +0300

    made solves into cards (wip)

--- src/lib/managing/Solve.svelte
+++ src/lib/managing/Solve.svelte
@@ -1,11 +1,36 @@
 <script lang='ts'>
 	import type { Solve } from '$lib/storage/time_db'
-
+	import { Card, CardText, CardActions, Button } from 'svelte-materialify'
 	export let solve: Solve;
+
+	let active: boolean;
+	let open = () => active = true
+
+	let penalty_name
+	if (solve.penalty === 0) {
+		penalty_name = 'None'
+	} if (solve.penalty === 2) {
+		penalty_name = '+2'
+	} if (solve.penalty === 'DNF') {
+		penalty_name = 'DNF'
+	}
+
 </script>
 
 <div>
-	Time is {solve.time.toFixed(2)}, date is {solve.date.getDate()}th, {solve.date.getMonth()} {solve.date.getFullYear()}, scrmable is {solve.scramble}
+	<div class="d-flex justify-center mt-4 mb-4">
+		<Card outlined style="max-width:300px;">
+		  <div class="pl-4 pr-4 pt-3">
+			  {#if solve.penalty !== 0}
+				  
+			  <span class="text-overline"> OVERLINE </span>
+			  {/if}
+			<br />
+			<span class="text-h5 mb-2">{solve.time.toFixed(3)}</span>
+			<br />
+		  </div>
+		</Card>
+	  </div>
 </div>
 
 <style>
