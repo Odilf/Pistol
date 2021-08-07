376aab4b7ae733b9324b08e355275db77f477a35
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:41:13 +0300

    added placehodler QuickStats component

---
+++ src/lib/timer/QuickStats.svelte
@@ -0,0 +1,13 @@
+<p>
+	Yee-haaaaaaa going fast :sungals:
+</p>
+
+<style lang="scss">
+	p {
+		font-size: 1.2em;
+		margin-top: 0;
+		margin-bottom: 1em;
+
+		color: darken(whitesmoke, 50%);
+	}
+</style>
