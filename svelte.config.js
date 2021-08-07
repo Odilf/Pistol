08a7203f46884912e90c646a964e8155d3abdb61
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:38:14 +0300

    re-installed css stuff

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
+	
 	preprocess: preprocess({
 		scss: {
 			includePaths: ['theme'],
