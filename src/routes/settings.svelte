6d033ba527ee20e60eef6bb3bfc5282169d158f0
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:42:47 +0300

    added settings page

---
+++ src/routes/settings.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="content">
+	<h1>Odilf Timer</h1>
+
+	<p>
+		And open source speedcubing timer, built with Svelte. 
+	<hr>
+	<p>
+		Still in development.
+	</p>
+
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+
+	p {
+		text-align: center;
+	}
+
+	hr {
+		border-width: 0;
+		background-color: black;
+		height: 0.1em;
+	}
+</style>
