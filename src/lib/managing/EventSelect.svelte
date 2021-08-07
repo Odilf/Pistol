5c3804b95f74bbf8d0074eba00c8b1878d19c7c7
Odilf <odysseas.maheras@gmail.com>
2021-08-07 11:39:25 +0300

    Separated event and session selectors

--- src/lib/managing/EventSelect.svelte
+++ src/lib/managing/EventSelect.svelte
@@ -1,38 +1,17 @@
 <script lang='ts'>
-	import { writable } from "svelte/store";
-	import { active_event, active_session, selection, wca_events } from "$lib/storage/time_db";
-	import { Tabs, Tab, Menu, List, ListItem, Button } from 'svelte-materialify'
+	import { selection, wca_events } from "$lib/storage/time_db";
+	import { Tabs, Tab } from 'svelte-materialify'
 
 	//Get events and add a option for a custom one
 	//TODO: #4 Add custom events
 	let selectable_events: String[] = [...wca_events, 'Add custom...']
 
-	$: selectable_sessions = $active_event.sessions.map(v => v.name)
-	
 </script>
 
-<div class="caca">
-	<Tabs centerActive bind:value={$selection.event}>
-		<div slot='tabs'>
-			{#each selectable_events as event }
-			<Tab>{event}</Tab>
-			{/each}
-		</div>
-	</Tabs>
-</div>
-
-<!-- Selected event is {$active_event.name}. Available sessions are: {selectable_sessions}
-Selected session is {$active_session.name} -->
-
-<div>
-	<Menu >
-		<div slot="activator">
-			<Button>{$active_session.name}</Button>
-		</div>
-		<List>
-			{#each selectable_sessions as session, i}
-				<ListItem on:click={() => $selection.sessions[$selection.event] = i}>{session}</ListItem> 
-			{/each}
-		</List>
-	</Menu>
-</div>
+<Tabs centerActive bind:value={$selection.event}>
+	<div slot='tabs'>
+		{#each selectable_events as event }
+		<Tab>{event}</Tab>
+		{/each}
+	</div>
+</Tabs>
---
+++ src/lib/managing/SessionSelect.svelte
@@ -0,0 +1,29 @@
+<script lang='ts'>
+	import { active_event, active_session, selection } from "$lib/storage/time_db";
+	import { Menu, List, ListItem, Button } from 'svelte-materialify'
+
+	$: selectable_sessions = $active_event.sessions.map(v => v.name)
+	
+</script>
+
+<body>
+	<!-- TODO: #30 Make hover on desktop but not require two clicks on mobile -->
+	<Menu>
+		<div slot="activator">
+			<Button>{$active_session.name}</Button>
+		</div>
+		<List>
+			{#each selectable_sessions as session, i}
+				<ListItem on:click={() => $selection.sessions[$selection.event] = i}>{session}</ListItem> 
+			{/each}
+		</List>
+	</Menu>
+</body>
+
+<style lang='scss'>
+	body {
+		padding: 1em;
+		height: auto;
+		width: auto;
+	}
+</style>
