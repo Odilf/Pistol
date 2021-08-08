961f96b89ef8557259342cdd9150cf110308651b
Odilf <odysseas.maheras@gmail.com>
2021-08-08 12:38:41 +0300

    fully implemented settings system

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,5 +1,5 @@
-import { writable } from 'svelte/store'
-
+import { browser } from '$app/env'
+import { writable } from 'svelte-local-storage-store'
 interface Setting {
 	readonly name: string
 	readonly description?: string
@@ -30,30 +30,42 @@ interface Category {
 }
 
 const default_settings: Category[] = [
-	{ name: 'Timing', description: 'Timing related stuff', settings: [
-		{name: 'Input method', description: 'How you input your times', value: 0, options: ['Timer', 'Typing'], child: 
-			{name: 'Input format', value: 0, options: ['ss.dd', 'mm:ss', 'mm:ss.dd'], parent: 1}},
-		{name: 'Decimals', description: 'Decimals to display in big timer', value: 2, options: ['0', '1', '2', '3']},
-	]},
+		{ name: 'Timing', description: 'Timing related stuff', settings: [
+			{name: 'Input method', description: 'How you input your times', value: 0, options: ['Timer', 'Typing'], child: 
+				{name: 'Input format', value: 0, options: ['ss.dd', 'mm:ss', 'mm:ss.dd'], parent: 1}},
+			{name: 'Decimals', description: 'Decimals to display in big timer', value: 2, options: ['0', '1', '2', '3']},
+			{name: 'Solve decimals', description: 'Decimals to display in solve list', value: 2, options: ['1', '2', '3']},
+		]},
 
-	{ name: 'Visual', description: 'How the timer looks', settings: [
-		{name: 'Theme', description: 'What the colors look like', value: 0, options: ['Dark', 'Light', 'Estonia']},
-		{name: 'Welp', description: 'This is a checkbox', value: false},
-	]},
+		{ name: 'Visual', description: 'How the timer looks', settings: [
+			{name: 'Theme', description: 'What the colors look like', value: 0, options: ['Dark', 'Light', 'Estonia']},
+			{name: 'Welp', description: 'This is a checkbox', value: false},
+		]},
 
-	{ name: 'Accesibility', description: 'Peepee poopoo idk what to put here', settings: [
-		{name: 'Button size', description: 'Make buttons bigger or smth', value: 1, min: 0, max: 3},
-	]},
+		{ name: 'Accesibility', description: 'Peepee poopoo idk what to put here', settings: [
+			{name: 'Button size', description: 'Make buttons bigger or smth', value: 1, min: 0, max: 3},
+		]},
 ]
 
-export const settings = writable(default_settings)
+export const settings = writable('settings', default_settings)
 
-export function getIndexOfSetting(setting_name: string): [number, number] {
-	for (const [i, category] of default_settings.entries()) {
-		for (const [j, setting] of category.settings.entries()) {
-			if (setting.name === setting_name) {
-				return [i, j]
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function getSettingByName(name: string) {
+	if (!browser) return null
+	const settings = JSON.parse(localStorage.getItem('settings'))
+
+	for (const category of settings) {
+		for (const setting of category.settings) {
+			if (setting.name === name) {
+				if ('options' in setting) { return setting.options[setting.value] }
+				else { return setting.value }
 			}
 		}
 	}
+}
+
+export function resetSettings(): void {
+	if (!browser) return
+	settings.set(null)
+	settings.set(default_settings)
 }
