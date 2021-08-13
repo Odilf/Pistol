32affe6b3afa7a20d9fc66aa87e465e2c579756c
Odilf <odysseas.maheras@gmail.com>
2021-08-13 20:37:02 +0300

    added about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -14,36 +14,104 @@
 	export const prerender = true;
 </script>
 
+<script lang="ts">
+	import Back from '$lib/utils/Back.svelte'
+	import '../app.css'
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<div class="content">
-	<h1>Odilf Timer</h1>
+<body>
+	<article>
+		
+		<header>
+			<Back></Back>
+		</header>
+		
+		<main>
+			<h1>Pistol</h1>
+			<p>Pistol is an open source speedcubing timer that brings a modern aproach and desing. <br>
+				Focused on the best possible user expirience: elegancy, simplicity, ease of use, performance and customization. <br> 
+				Built with <a href='https://kit.svelte.dev'>SvelteKit</a>, still under development.
+			</p>
 
-	<p>
-		And open source speedcubing timer, built with Svelte. 
-	<hr>
-	<p>
-		Still in development.
-	</p>
+			<h2>Contributing</h2>
+			<hr>
+			<p>
+				If you found a typo or a easy issue to fix, feel free to raise a new issue. 
+				I will fix it if it's reasonable or I will close it if it's not. <br>
+				If anyone wants to implement a new feature or to make changes in the code themselves, 
+				I will <i>gladly</i> review and merge pull requests. 
+				Just don't expect me to make the development process easier (I have no clue what I would be suppossed to do).
+			</p>
 
-</div>
+			<h2>Credits</h2>
+			<hr>
+			<p>
+				All the cubing visualizations were made with <a href=https://js.cubing.net/cubing>cubing.js</a>. <br>
+				Timer inspired mainly by <a href=https://cstimer.net>csTimer</a> and partialy by <a href=https://timer.cubing.net>timer.cubing.net/</a>.
+			</p>
 
-<style>
-	.content {
+			<h2>License</h2>
+			<hr>
+			<p>
+				This project is licensed under the GPL license (version 3 or later). 
+				This means that this library is free to use, although you must publish any code that uses it (e.g. also put it on GitHub). <br> 
+				See the <a href=https://www.gnu.org/licenses/gpl-3.0.en.html>exact license</a> for exact details.
+			</p>
+		</main>
+	</article>
+</body>
+	
+<style lang='scss'>
+	header {
+		display: flex;
+		align-items: center;
 		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
+	}
+	h1 {
+		font-size: 3.5em;
+		margin-bottom: 1rem;
+		font-weight: 700;
 	}
 
+	h2 {
+		font-size: 2em;
+		margin-top: 1rem;
+		font-weight: 500;
+	}
+
+	main {
+		max-width: 700px;
+	}
+	
+	body {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		background-color: #4b0000;
+	}
+	article {
+		padding: 2em;
+		min-height: 100%;
+		width: min(90%, 1200px);
+		background-color: var(--primary-color);
+
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+
+		overflow: scroll;
+	}
 	p {
-		text-align: center;
+		margin-top: 0.5rem;
 	}
-
-	hr {
-		border-width: 0;
-		background-color: black;
-		height: 0.1em;
+	a {
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
 	}
 </style>
