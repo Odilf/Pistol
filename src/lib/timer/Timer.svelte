b816fc561ef4526ec87f88d22d8d24f6995d0079
Odilf <odysseas.maheras@gmail.com>
2021-08-13 19:56:11 +0300

    Added time press delay

--- src/lib/timer/Timer.svelte
+++ src/lib/timer/Timer.svelte
@@ -2,33 +2,39 @@
 
 <script lang='ts'>
 	import { createEventDispatcher } from 'svelte';
+	import { getSettingByName } from '$lib/settings';
 	import { addSolve, selection } from '$lib/storage/time_db'
 	import type { Solve } from '$lib/storage/time_db'
 	import RunningTime from './RunningTime.svelte';
 	
 	const dispatch = createEventDispatcher();
 	
+	$: delay_time = parseFloat(getSettingByName('Timer delay'))
+	let press_start: Date
+	let delay_interval: ReturnType<typeof setInterval>
 	let timerState = 'released'
 	let time = 0
 	let interval: ReturnType<typeof setInterval>
 	let startTime: Date
 	
 	function handleKeydown(e) {
-		
 		//Pressed to prepare timer
 		if (timerState === 'released' && e.code === 'Space') { 
 			dispatch('timerReady', { time: time });
 			timerState = 'pressed'
-			
+			press_start = new Date()
+			if (delay_time === 0) { timerState = 'ready'} 
+			else { delay_interval = setInterval(() => timerState = 'ready', delay_time * 1000) }
+					
 		//Pressed to stop timer
 		} else if (timerState === 'running') {
-			time = getTime(startTime, new Date())
+			time = getTime(startTime)
 			dispatch('timerEnd', { time: time });
 			
-			timerState = 'released'
+			timerState = 'pressed'
 			clearInterval(interval)
 
-			let solve: Solve = {time: time, penalty: 0, date: null, scramble: 'I h8 our l1f3', reconstruction: null}
+			let solve: Solve = {time: time, penalty: 0, date: null, scramble: "B2 D2 F2 D2 B R2 B L2 D2 R2 B R U F2 U' R' D2 L B R'", reconstruction: null}
 			//TODO: #6 Is the selection store a good idea?
 			addSolve(solve, $selection.event, $selection.sessions[$selection.event])
 		}
@@ -38,17 +44,22 @@
 		
 		//Released to start timer
 		if (e.code === 'Space') {
-			if (timerState === 'pressed') {
+			if (timerState === 'ready') {
 				dispatch('timerStart', { time: time });
 				timerState = 'running'
 				startTime = new Date()
 				//TODO: #5 Add option to change timer refresh rate
-				interval = setInterval(() => time = getTime(startTime, new Date()), 42)
+				interval = setInterval(() => time = getTime(startTime), 42)
+			} else {
+				timerState = 'released'
 			}
 		}
+
+		clearInterval(delay_interval)
 	}
 	
-	function getTime(start: Date, end: Date) {
+	
+	function getTime(start: Date, end = new Date()) {
 		return (end.getTime() - start.getTime())/ 1000
 	}
 	
@@ -79,6 +90,9 @@
 	}
 	.running {
 		color: lightblue; 
+	}
+	.ready {
+		color: greenyellow
 	}
 	
 </style>
