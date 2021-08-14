c3533c591e8a94e1b6c20f3f1ea5c931897a404b
Odilf <odysseas.maheras@gmail.com>
2021-08-14 11:35:45 +0300

    fixed button spacing and width to match

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,18 +1,15 @@
 <script>
 	import '../app.css'
 	import { Footer, Button } from 'svelte-materialify'
-	const links = [
-		{name: 'Settings', url: '/settings'},
-		{name: 'About', url: '/about'},
-		{name: 'GitHub', url: 'https://github.com/Odilf/Odilf-Timer'},
-		]
+
+	let wide = 'width:10em'
 </script>
 
 <Footer class='justify-center flex-column'>
 		<div class="buttons">
-			<Button on:click={() => location.href = '/settings'}> Settings </Button>
+			<Button style={wide} on:click={() => location.href = '/settings'}> Settings </Button>
 			<Button on:click={() => window.open('https://github.com/Odilf/Odilf-Timer','_blank')}> <img src='static/github.svg' alt='GitHub'/> </Button>
-			<Button on:click={() => location.href = '/about'}> About</Button>
+			<Button style={wide} on:click={() => location.href = '/about'}> About</Button>
 		</div>
 	<p class="text-center" style="width:100%">
 		2021&nbsp;-&nbsp;<b>Odilf</b>
