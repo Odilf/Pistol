cd0e7c4048b56a418d92c59e6a319a8ee4962a60
Odilf <odysseas.maheras@gmail.com>
2021-09-13 19:28:10 +0200

    for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,12 +12,9 @@ const config = {
 		},
 	}),
 
-	// kit: {
-	// 	// hydrate the <div id="svelte"> element in src/app.html
-	// 	target: '#svelte'
-	// }
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
