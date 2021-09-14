5cd5e086df2abe2c4e7af9c60f30e50954bac680
Odilf <odysseas.maheras@gmail.com>
2021-09-14 21:05:23 +0200

    added keyboard navigation through events

--- src/lib/navigation/EventSelect.svelte
+++ src/lib/navigation/EventSelect.svelte
@@ -4,9 +4,22 @@
 
 	//Get events and add a option for a custom one
 	//TODO: #4 Add custom events
-	let selectable_events: String[] = [...wca_events, 'Add custom...']
+	// let selectable_events: String[] = [...wca_events, 'Add custom...']
+	let selectable_events: String[] = [...wca_events]
+
+	// Navigation with arrows
+	function handleKeydown(e) {
+		if (e.code === 'ArrowLeft') {
+			($selection.event == 0) || ($selection.event -= 1)
+		}
+		if (e.code === 'ArrowRight') {
+			$selection.event < selectable_events.length - 1 && ($selection.event += 1)
+		}
+	}
 
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <Tabs centerActive bind:value={$selection.event}>
 	<div slot='tabs'>
