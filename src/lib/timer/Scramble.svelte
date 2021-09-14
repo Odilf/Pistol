f84a7900880cb89d5776120ff07f0448109cc9f7
Odilf <odysseas.maheras@gmail.com>
2021-09-14 21:46:44 +0200

    added margin to scramble

--- src/lib/timer/Scramble.svelte
+++ src/lib/timer/Scramble.svelte
@@ -9,9 +9,16 @@
 
 <style lang="scss">
 	p {
+		height: 3em;
 		font-size: 1.2em;
 		// margin: 200px, 100px, 0;
 		margin-top: 1em;
 		margin-bottom: 0;
+		margin-left: 1em;
+		margin-right: 1em;
+
+		// Text centering
+		display: flex;
+		justify-content: center;
 	}
 </style>
