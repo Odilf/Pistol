5263d34e1ec0416cf404741f922014b4211c6bd8
Odilf <odysseas.maheras@gmail.com>
2021-09-16 23:16:55 +0200

    made a derived selectable_sessions store and minor cleanup

--- src/lib/storage/time_db.ts
+++ src/lib/storage/time_db.ts
@@ -1,11 +1,10 @@
 import { browser } from "$app/env";
-import { writable as LS_writable } from "svelte-local-storage-store";
+import { writable } from "svelte-local-storage-store";
 import { derived } from 'svelte/store'
 
 export const wca_events = ['3x3', '2x2', '4x4', '5x5', '6x6', '7x7', '3BLD', 'FMC', 'OH', 'Clock', 
-'Megaminx', 'Pyraminx', 'Skewb', 'Sq-1', '4BLD', '5BLD', 'MBLD']
+							'Megaminx', 'Pyraminx', 'Skewb', 'Sq-1', '4BLD', '5BLD', 'MBLD']
 
-//TODO: #3 Add correct default se	ssions for each event
 const default_sessions_for_event = [
 	{name: '3x3', sessions: ['Main', 'PLL', 'Last layer', 'Alg drill']},
 	{name: '2x2', sessions: ['Main', 'Alg drill']},
@@ -27,7 +26,8 @@ const default_sessions_for_event = [
 ]
 
 //Change for non-testing purposes
-const default_session: Session = { name: 'Default (test)', solves: [
+const default_session: Session = {
+ name: 'Default (test)', solves: [
 	{time: 4.2, penalty: 0, date: new Date(), scramble: "R U R' U'", reconstruction: ''},
 	{time: 5, penalty: 2, date: new Date(), scramble: "I F UR MOM", reconstruction: ''},
 	{time: 0.69, penalty: 'DNF', date: new Date(), scramble: "M' S M S'", reconstruction: ''}
@@ -48,29 +48,35 @@ export type Session = {
 
 export type Event = {
 	name: string
+	hide: boolean
 	sessions: Session[]
 };
 
 function build_default_database(): Event[] {
-	let events: Event[] = []
+	const events: Event[] = []
+	// Build default sessions if not specified
 	for (const event of wca_events) {
 		// i is -1 if default sessions haven't been specified for event
 		const i = default_sessions_for_event.map(v => v.name).indexOf(event)
 		const sessions = i === -1 
 		? [default_session] 
-		: default_sessions_for_event[i].sessions.map( (v, i) => ({id: i, name: v, solves: []}) )
+		: default_sessions_for_event[i].sessions.map( v => ({name: v, solves: []}) )
 		
-		events = [...events, {name: event, sessions: sessions} ]
+		events.push( {name: event, hide: false, sessions: sessions} )
 	}
 	return events
 }
 
-const default_events = build_default_database()
+export const database = writable('database', build_default_database())
 
-export const database = LS_writable('database', default_events)
+//Object with selected event index and array of selected sessions index for each event (defaults to 0)
+export const selection = writable('selection', {event: 0, sessions: wca_events.map(() => 0)})
+selection.update(() => { return {event: 0, sessions: wca_events.map(() => 0)}})
 
-//Object with selected event id and array of selected sessions id for each event (defaults to 0)
-export const selection = LS_writable('selection', {event: 0, sessions: wca_events.map(() => 0)})
+export const selectable_events = derived(
+	database,
+	$database => $database.filter(v => v.hide === false)
+)
 
 //Get active event and session from the selection store
 export const active_event = derived(
@@ -79,23 +85,28 @@ export const active_event = derived(
 )
 
 export const active_session = derived(
-	[selection, database],	
-	([$selection, $database]) => $database[$selection.event].sessions[$selection.sessions[$selection.event]]
+	[selection, active_event],	
+	([$selection, $active_event]) => $active_event.sessions[$selection.sessions[0]]
 )
+
+// export const active_session = derived(
+// 	[selection, database],	
+// 	([$selection, $database]) => {
+// 		console.log($selection[2])
+// 		return $database[$selection.event].sessions[$selection.sessions[$selection.event]]
+// 	}
+// )
 
 let active_session_value: Session
 active_session.subscribe(v => active_session_value = v)
 
-export function addSolve(solve: Solve): void {
 
+export function addSolve(solve: Solve): void {
 	if (!solve.date) {
 		solve.date = new Date()
 	}
 
-	// database.update(db => {
-		active_session_value.solves.push(solve)
-	// 	return db
-	// })
+	active_session_value.solves.push(solve)
 }
 
 export function deleteAllSolves(): void {
