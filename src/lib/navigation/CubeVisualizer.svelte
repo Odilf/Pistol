18b26745c87284ce529d0ce2d4c9c24977a7bcb9
Odilf <odysseas.maheras@gmail.com>
2021-09-19 19:41:33 +0200

    added cube visualizer support for all cubing.js cubes

--- src/lib/navigation/CubeVisualizer.svelte
+++ src/lib/navigation/CubeVisualizer.svelte
@@ -1,25 +1,57 @@
 <script lang='ts'>
 	import type { Solve } from '$lib/storage/database'
 	import { TwistyPlayer } from "cubing/twisty"; //Necessary for cube visualization
-	export let solve: Solve;
+import { loop_guard } from 'svelte/internal';
 	import '../../app.css'
+
+	export let solve: Solve;
+	export let puzzle: string;
+
+	const twisty_dict = {
+		'3x3x3': ['3x3', '3BLD', 'FMC', 'OH'],
+		'2x2x2': ['2x2'],
+		'4x4x4': ['4x4', '4BLD'],
+		'5x5x5': ['5x5', '5BLD'],
+		'6x6x6': ['6x6'],
+		'7x7x7': ['7x7'],
+		'megaminx': ['Megaminx'],
+		'pyraminx': ['Pyraminx'],
+		'skewb': ['Skewb'],
+		'': ['Clock', 'MBLD', 'Other'],
+	}
+
+	for (const [twisty_name, pistol_name] of Object.entries(twisty_dict)) {
+		if (pistol_name.includes(puzzle)) {
+			puzzle = twisty_name
+		}
+	}
+	
 
 	$: if (!solve.reconstruction) {
 		solve.reconstruction = ''
 	}
+
+	if (puzzle === '3x3') {
+		solve.scramble = `${solve.scramble} x2 y2 `
+	}
+
+	
 	
 </script>
 
+{#if puzzle}
+
 <twisty-player
-	alg={`${solve.scramble} x2 y2 ${solve.reconstruction}`}
+	{puzzle}
+	alg={`${solve.scramble} ${solve.reconstruction}`}
 	background="none"
 	hint-facelets="none"
 	control-panel="none"
 	/>
-	
 <p>
 	Cube visualization courtesy of cubing.js
 </p>
+{/if}
 
 <style>
 
