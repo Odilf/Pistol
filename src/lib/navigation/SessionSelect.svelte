8e4cd6c20dabb3554d276af40db14c6e036d1114
Odilf <odysseas.maheras@gmail.com>
2021-09-21 09:37:45 +0200

    Made nice card for adding sessions

--- src/lib/navigation/SessionSelect.svelte
+++ src/lib/navigation/SessionSelect.svelte
@@ -1,7 +1,7 @@
 <script lang='ts'>
 	import type { Event } from '$lib/storage/database'
 	import { addSession } from '$lib/storage/database'
-	import { Menu, List, ListItem, Button, Dialog } from 'svelte-materialify'
+	import { Menu, List, ListItem, Button, Dialog, Card, CardTitle, CardText, CardActions } from 'svelte-materialify'
 
 	export let event: Event
 	let adding_session: boolean
@@ -25,13 +25,25 @@
 </body>
 
 <Dialog bind:active={adding_session}>
-	Enter session name
-	<input bind:value={new_session_name}/>
-	<Button on:click={() => {
-		addSession(new_session_name, event);
-		adding_session = false;
-		event.selected_session = event.sessions.length - 1
-	}}> Done </Button>
+	<Card>
+		<CardTitle>
+			Enter session name
+		</CardTitle>
+
+		<CardText>
+			<!-- svelte-ignore a11y-autofocus -->
+			<input bind:value={new_session_name} autofocus/>
+		</CardText>
+
+		<CardActions>
+			<Button color=red> Cancel </Button>
+			<Button color='blue justify-right' on:click={() => {
+				addSession(new_session_name, event);
+				adding_session = false;
+				event.selected_session = event.sessions.length
+			}}> Done </Button>
+		</CardActions>
+	</Card>
 </Dialog>
 
 <style lang='scss'>
@@ -45,5 +57,18 @@
 	div {
 		flex-grow: 1;
 		display: flex;
+	}
+	input {
+		background-color: var(--secondary-color);
+		color: whitesmoke;
+		width: 80%;
+		padding: 0.3em;
+		font-size: 2em;
+		border-radius: 5px;
+	}
+	input:focus {
+		// background-color: var(--accent-color);
+		outline:1000px solid white;
+		outline: none;
 	}
 </style>
