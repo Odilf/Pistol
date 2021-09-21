622848c71ca0cfba2b151a265e746ac4fa353af2
Odilf <odysseas.maheras@gmail.com>
2021-09-21 22:40:57 +0200

    added `event.scramble` guard

--- src/lib/storage/versionManager.ts
+++ src/lib/storage/versionManager.ts
@@ -1,5 +1,18 @@
 import { browser } from "$app/env"
 
+import { database, wca_events } from "./database"
+
+database.update(db => {
+	db.events.map(event => {
+		if (wca_events.includes(event.name) && !event.scramble) {
+			console.log('cac');
+			
+			event.scramble = event.name
+		}
+	})
+	return db
+})
+
 if (browser) {
 	const version = localStorage.getItem('version')
 
