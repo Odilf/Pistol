97e1c076a176dedc8373f33bf166fab7a56dbbfc
Odilf <odysseas.maheras@gmail.com>
2021-09-22 13:47:06 +0200

    removed `TimeChart.svelte`

--- src/lib/dashboard/DashboardCard.svelte
+++ src/lib/dashboard/DashboardCard.svelte
@@ -1,8 +1,7 @@
 <script lang='ts'>
 	import { Card, CardTitle, CardSubtitle, CardText, CardActions } from 'svelte-materialify'
 	import type { Event, Session, Solve } from '$lib/storage/database'
-
-	import TimeChart from './TimeChart.svelte'
+	
 	import * as stats from './statistician'
 
 	export let event: Event
@@ -16,5 +15,4 @@
 	{stats.get_reduced_sample(session.solves.map(v => v.time), 20)}
 	<CardTitle> {event.name} </CardTitle>
 	<CardSubtitle> {average} </CardSubtitle>
-	<TimeChart {session}/>
 </Card>
