57a1b63cb374662a36ef1f597eb351f7764b9005
Odilf <odysseas.maheras@gmail.com>
2021-11-26 10:27:31 +0100

    removed `console.log`

--- src/lib/utils/KeyboardNav.svelte
+++ src/lib/utils/KeyboardNav.svelte
@@ -1,7 +1,6 @@
 <script>
 	function handleKeydown(e) {
 		if (e.shiftKey) {
-			console.log(e.key);
 			if (e.key === 'H' || e.key === 'T') { location.href = '/' }
 			if (e.key === 'S') { location.href = '/settings' }
 			if (e.key === 'D') { location.href = '/dashboard' }
