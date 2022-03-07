e266499d51561e2b9ad042e0b41606c8605c781e
Odilf <odysseas.maheras@gmail.com>
2022-03-07 09:45:41 +0100

    innitial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+	fontFamily: {
+		'sans': ['inter var', 'ui-sans-serif', 'system-ui', ]
+	},
+    extend: {},
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
