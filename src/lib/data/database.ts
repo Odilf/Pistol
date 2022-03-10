396e145677770078c7b5147bb03c4886deb60142
Odilf <odysseas.maheras@gmail.com>
2022-03-10 23:14:52 +0100

    made firebase access stuff

--- src/lib/data/database.ts
+++ src/lib/data/database.ts
@@ -1,11 +1,22 @@
 import { app } from './firebase'
-import { getDatabase, connectDatabaseEmulator, ref, onValue, set, push, query, orderByChild, orderByKey, endAt, startAt, limitToLast } from "firebase/database";
+import { getDatabase, connectDatabaseEmulator, ref, onValue, set, query, orderByKey, limitToLast, get } from "firebase/database";
 import { browser } from "$app/env";
-import type { Session, Solve, Event } from './architecture';
+import { type Session, type Solve, type Event, defaultEvents } from './architecture';
 import { writable } from 'svelte/store';
-import { debug } from 'svelte/internal';
+import type { Writable } from 'svelte/store';
+
+import { user } from '$lib/user';
 
 export const db = browser && getDatabase(app);
+
+let uid = null
+user.subscribe(user => {
+	uid = user && `User-${user.uid}`
+})
+
+const solvesPrefix = 'Solves'
+const eventsPrefix = 'Events'
+const settingsPrefix = 'Settings'
 
 type Without<T, K> = {
     [L in Exclude<keyof T, K>]: T[L]
@@ -39,21 +50,39 @@ if (browser) {
 }
 
 export async function addSolve(solve: Solve, session: Session, event: Event) {
-	const sessionRef = ref(db, `solves/${event.name}/${session.name}/${solve.date.getTime()}`)
+	const sessionRef = ref(db, `${uid}/${solvesPrefix}/${event.name}/${session.name}/${solve.date.getTime()}`)
 	delete solve.date
 
 	await set(sessionRef, solve)
 }
 
-export function getSolves(session: Session, event: Event, amount: number = null) {
-	const sessionRef = ref(db, `solves/${event.name}/${session.name}`)
+export function getSolves(session: Session, event: Event, amount: number = null): Writable<Solve[]> {
+	if (!db) {
+		console.log('Database isn\'t yet initialized');
+		return writable([])
+	}
+	const sessionRef = ref(db, `${uid}/${solvesPrefix}/${event.name}/${session.name}`)
 
 	const solvesQuery = amount ? query(sessionRef, orderByKey(), limitToLast(amount)) : query(sessionRef, orderByKey())
 
-	
+	const solves = writable([] as Solve[])
+
 	onValue(solvesQuery, snapshot => {
 		solves.set(reshapeSolves(snapshot.val()))
 	})
+
+	return solves
 }
 
-export const solves = writable([] as Solve[])
+export async function getUserEvents(): Promise<Event[]> {
+	const eventRef = ref(db, `${uid}/${eventsPrefix}`)
+	let snapshot = await get(eventRef)
+	const events = snapshot.val() as Event[]
+
+	if (!events) {
+		console.log('No events!');
+		await set(eventRef, defaultEvents)
+	}
+
+	return defaultEvents
+}
