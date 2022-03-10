6197ea3e3b9eea027ea47ef57c392c72d06f2d5d
Odilf <odysseas.maheras@gmail.com>
2022-03-10 23:14:31 +0100

    changed colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,16 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-	colors: {
-		'primary': 'white',
-		'secondary': '#385F71',
-		'black': '#242224',
-		'accent': '#8F754F',
-		// 'accent-secondary': '#8F754F',
-	},
 	fontFamily: {
 		'sans': ['InterVariable', 'ui-sans-serif', 'system-ui', ],
 		'mono': ['Roboto Mono'],
 	},
-    extend: {},
+    extend: {
+		colors: {
+			'primary': '#E2E8FD',
+			'secondary': '#151E3F',
+		},
+	},
   },
 
   plugins: [],
