dae7be87bc6fbcf9ba15762055ba0e9c36575a96
Odilf <odysseas.maheras@gmail.com>
2022-03-11 11:35:20 +0100

    renamed event tabs

--- src/lib/UI/EventTabs.svelte
+++ src/lib/UI/EventTabs.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
 	import type { Event, Session } from "$lib/data/architecture";
-	import { createEventDispatcher } from "svelte";
-
-	const dispatch = createEventDispatcher()
 	
 	export let events: Event[]
-	export let selected = 0
+	let selected = 0
 
-	function sendClick(selected: number) {
-		const event = events[selected]
-		dispatch('change', {
-			event, 
-			session: event.sessions[0]
-		})
+	export let selection: { event: Event; session: Session; }
+	$: selection = getSelection(selected, 0)
+
+	function getSelection(selectedEvent, selectedSession) {
+		const event = events[selectedEvent]
+		const session = event.sessions[selectedSession]
+		return { event, session }
 	}
-
-	$: sendClick(selected)
 
 	function handleKeydown(e: KeyboardEvent) {
 		if (e.key === 'ArrowLeft') {
@@ -29,12 +25,12 @@
 
 <svelte:window on:keydown={handleKeydown}/>
 
-<div class='flex rounded p-2 bg-secondary text-primary shadow-xl w-fit mx-auto'>
+<div class='flex rounded p-2 bg-secondary text-primary shadow-lg w-fit mx-auto max-w-full overflow-x-scroll'>
 	{#each events as event, i}
 	<button class='p-2 rounded transition px-4 mx-1 clickable
 	{selected === i ? 'bg-primary text-secondary shadow' : ''}'
 	on:click={() => selected = i}>
-		<div>
+		<div class='font-bold text-lg'>
 			{event.abbreviation}
 		</div>
 	</button>
