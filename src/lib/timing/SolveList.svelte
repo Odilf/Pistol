3ec53b4ab34be981cbdadfb658df52e0ad50ca81
Odilf <odysseas.maheras@gmail.com>
2022-03-11 11:35:12 +0100

    improves solveList

--- src/lib/timing/SolveList.svelte
+++ src/lib/timing/SolveList.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
 	import type { Solve } from "$lib/data/architecture";
 	import { flip } from "svelte/animate";	
+	import { fly } from "svelte/transition";	
 	import TimeDisplay from "./TimeDisplay.svelte";
 
 	export let solves: Solve[]
 
+	let duration = 400
+	export { duration as transitionDuration }
+
 	$: length = Math.max(solves.length, 8)
 </script>
 
-<div class='flex flex-col items-center'>
-	{#each solves.reverse() as solve, i (solve.date.getTime())}
-	<div style:opacity={ 1 - ((i + 1)/length)} animate:flip class='transition'>
-		<TimeDisplay time={solve.time} />
-	</div>
-	{/each}
+<div class='flex flex-col text-lg items-center'>
+	{#if solves.length}
+		{#each solves.reverse() as solve, i (solve.date.getTime())}
+			<div style:opacity={ 1 - ((i)/length)} animate:flip={{ duration }} in:fly|local={{y: -20, duration }} class='transition'>
+				<TimeDisplay time={solve.time} />
+			</div>
+		{/each}
+	{:else}
+		<p class='opacity-30'>
+			No solves yet. 
+			<br>
+			Start solving!
+		</p>
+	{/if}
 </div>
