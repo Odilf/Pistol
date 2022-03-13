d2de1f8916308b66c8111fb4dff290eff636ef3c
Odilf <odysseas.maheras@gmail.com>
2022-03-13 12:30:26 +0100

    added a cube Visualiser

---
+++ src/lib/UI/CubeVisualiser.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { TwistyPlayer } from 'cubing/twisty'
+	import { onMount } from 'svelte';
+	
+	let container: HTMLDivElement
+
+	const twistyPlayer = new TwistyPlayer({
+		alg: "R U R' U'",
+		background: 'none',
+		controlPanel: 'none',
+	})
+
+	onMount(() => container.appendChild(twistyPlayer))
+
+</script>
+
+<div bind:this={container}/>
