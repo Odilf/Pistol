3fcc3c35a009a46bac0b3774137ffcb776149eb7
Odilf <odysseas.maheras@gmail.com>
2022-03-13 18:27:15 +0100

    added a pretty fucking nice scramble visualizer

--- src/lib/UI/Scramble.svelte
+++ src/lib/UI/Scramble.svelte
@@ -1,23 +1,40 @@
 <script lang="ts">
-	import Hoverable from "$lib/utils/Hoverable.svelte";
+	import { spring } from 'svelte/motion'
 	import CubeVisualiser from "./CubeVisualiser.svelte";
 	export let scramble: string
 
 	$: moves = scramble?.split(' ') || ['']
-
 	let hovering: number
+
+	let x = spring(0, { stiffness: 0.1, damping: 0.5 })
+
+	let container: HTMLDivElement
+	let cubeWrapper: HTMLDivElement
+
+	$: containerRect = container?.getBoundingClientRect()
+	$: cubeWrapperRect = cubeWrapper?.getBoundingClientRect()
+
+	$: if (hovering != null) {
+		$x = containerRect.width * hovering / moves.length
+	}
+
+	$: offset = containerRect?.x - cubeWrapperRect?.width/2
+	
+
 </script>
 
-{hovering}
 <div class='w-fit text-center mx-auto max-w-screen-md font-light {scramble.length > 100 ? 'text-lg' : 'text-2xl'} cursor-pointer'
-	on:mouseleave={() => hovering = undefined}>
+	on:mouseleave={() => hovering = undefined} bind:this={container}>
 	{#each moves as move, i}
-		<span class='{hovering >= i ? 'bg-slate-400/50' : ''} px-[1.5px] transition' on:mouseenter={() => hovering = i}>
+		<span class='{hovering >= i ? 'text-blue-400' : ''} mx-[1.5px] rounded transition' on:mouseenter={() => hovering = i}>
 			{move}
 		</span>
 	{/each}
 	{#if hovering != null}
-		<div class='absolute z-10' style:top={0} style:left={0}>
+		<div class='absolute z-10 opacity-80' 
+		style:left='{offset}px' style:transform='translateX({$x}px)'
+		bind:this={cubeWrapper}
+		>
 			<CubeVisualiser alg={scramble} move={hovering}/>
 		</div>	
 	{/if}
