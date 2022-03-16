5e33c36344f81f25cce30e93167412bb3852ece1
Odilf <odysseas.maheras@gmail.com>
2022-03-16 15:38:50 +0100

    maybe dynamic import?

--- src/lib/scramble/scrambler.ts
+++ src/lib/scramble/scrambler.ts
@@ -1,35 +1,48 @@
+import { browser } from "$app/env";
 import type { ScrambleType } from "$lib/data/architecture";
+import { src_url_equal } from "svelte/internal";
 // import { randomScrambleForEvent } from "cubing/scramble";
 
-// const scrambleTypeDict = {
-// 	'3x3': '333',
-// 	'2x2': '222',
-// 	'4x4': '444',
-// 	'5x5': '555',
-// 	'6x6': '666',
-// 	'7x7': '777',
+async function getCubingjsSrambler() {
+	const { randomScrambleForEvent } = await import('cubing/scramble') 
+	return randomScrambleForEvent
+}
 
-// 	'3BLD': '333bf',
+const scrambleTypeDict = {
+	'3x3': '333',
+	'2x2': '222',
+	'4x4': '444',
+	'5x5': '555',
+	'6x6': '666',
+	'7x7': '777',
 
-// 	'Clock': 'clock',
-// 	'Megaminx': 'minx',
-// 	'Pyraminx': 'pyram',
-// 	'Skewb': 'skewb',
-// 	'Square1': 'sq1',
+	'3BLD': '333bf',
 
-// 	'4BLD': '444bf',
-// 	'5BLD': '555bf',
-// 	'MBLD': '333mb',
-// }
+	'Clock': 'clock',
+	'Megaminx': 'minx',
+	'Pyraminx': 'pyram',
+	'Skewb': 'skewb',
+	'Square1': 'sq1',
 
-// export async function getRandomScramble(scrambleType: ScrambleType) {
-// 	const eventID = scrambleTypeDict[scrambleType]
-// 	if (!eventID) {
-// 		console.error('Unsuported event:', scrambleType);
-// 	}
-// 	return (await randomScrambleForEvent(eventID)).toString()
-// }
+	'4BLD': '444bf',
+	'5BLD': '555bf',
+	'MBLD': '333mb',
+}
+
+export let randomScrambleForEvent = async(scrambleType: ScrambleType) => {
+	return null
+}
+
+if (browser) {
+	getCubingjsSrambler().then(scrambler => {
+		randomScrambleForEvent = scrambler
+	})
+}
 
 export async function getRandomScramble(scrambleType: ScrambleType) {
-	return 'R U'
+	const eventID = scrambleTypeDict[scrambleType]
+	if (!eventID) {
+		console.error('Unsuported event:', scrambleType);
+	}
+	return (await randomScrambleForEvent(eventID)).toString()
 }
