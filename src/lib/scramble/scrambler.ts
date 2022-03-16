70d8f00228afa76dc51caef7e6c65d88d007fdf5
Odilf <odysseas.maheras@gmail.com>
2022-03-16 10:06:19 +0100

    is it the scrambler?

--- src/lib/scramble/scrambler.ts
+++ src/lib/scramble/scrambler.ts
@@ -1,31 +1,35 @@
 import type { ScrambleType } from "$lib/data/architecture";
-import { randomScrambleForEvent } from "cubing/scramble";
+// import { randomScrambleForEvent } from "cubing/scramble";
 
-const scrambleTypeDict = {
-	'3x3': '333',
-	'2x2': '222',
-	'4x4': '444',
-	'5x5': '555',
-	'6x6': '666',
-	'7x7': '777',
+// const scrambleTypeDict = {
+// 	'3x3': '333',
+// 	'2x2': '222',
+// 	'4x4': '444',
+// 	'5x5': '555',
+// 	'6x6': '666',
+// 	'7x7': '777',
 
-	'3BLD': '333bf',
+// 	'3BLD': '333bf',
 
-	'Clock': 'clock',
-	'Megaminx': 'minx',
-	'Pyraminx': 'pyram',
-	'Skewb': 'skewb',
-	'Square1': 'sq1',
+// 	'Clock': 'clock',
+// 	'Megaminx': 'minx',
+// 	'Pyraminx': 'pyram',
+// 	'Skewb': 'skewb',
+// 	'Square1': 'sq1',
 
-	'4BLD': '444bf',
-	'5BLD': '555bf',
-	'MBLD': '333mb',
-}
+// 	'4BLD': '444bf',
+// 	'5BLD': '555bf',
+// 	'MBLD': '333mb',
+// }
+
+// export async function getRandomScramble(scrambleType: ScrambleType) {
+// 	const eventID = scrambleTypeDict[scrambleType]
+// 	if (!eventID) {
+// 		console.error('Unsuported event:', scrambleType);
+// 	}
+// 	return (await randomScrambleForEvent(eventID)).toString()
+// }
 
 export async function getRandomScramble(scrambleType: ScrambleType) {
-	const eventID = scrambleTypeDict[scrambleType]
-	if (!eventID) {
-		console.error('Unsuported event:', scrambleType);
-	}
-	return (await randomScrambleForEvent(eventID)).toString()
+	return 'Cacaaaa lol'
 }
