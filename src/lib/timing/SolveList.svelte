f0683d93179bd59d551d92e4e4c120d7f3a3c462
Odilf <odysseas.maheras@gmail.com>
2022-05-08 10:50:00 +0200

    added keayboard delete and undo delete

--- src/lib/timing/SolveList.svelte
+++ src/lib/timing/SolveList.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-	import type { Event, Session, Solve } from "$lib/data/architecture";
+	import type { Solve, Selection } from "$lib/data/architecture";
+	import { addSolve, deleteSolve } from "$lib/data/database";
+	import Overlay from "$lib/UI/basic/Overlay.svelte";
 	import SolveCard from "$lib/UI/solves/SolveCard.svelte";
 	import SolveDisplay from "$lib/UI/solves/SolveDisplay.svelte";
+	import { getContext } from "svelte";
 
 	import { flip } from "svelte/animate";	
+	import type { Writable } from "svelte/store";
 	import { fly } from "svelte/transition";	
 
 	export let solves: Solve[]
-	export let selection: { session: Session, event: Event }
+	const selection = getContext('selection') as Writable<Selection>
+
+	$: if (!solves.includes(showingSolve)) {
+		showingSolve = null
+	}
 
 	export { duration as transitionDuration }
 	export const minSolveLength = 5
@@ -17,7 +25,28 @@
 	const getOpacity = (i: number) => 1 - (i/length)
 	
 	let showingSolve: Solve
+
+	// Keyboard delete solve and undo
+	let deletedSolves = []
+	function handleKeydown(e: KeyboardEvent) {
+		if (!$selection) return
+
+		if (e.code === 'KeyZ' && (e.altKey || e.metaKey)) {
+			console.log(deletedSolves);
+			
+			if (!e.shiftKey) {
+				const solve = solves.at(-1)
+				deleteSolve(solve, $selection)
+				deletedSolves.push(solve)
+			} else if (deletedSolves.length > 0) {
+				addSolve(deletedSolves.at(-1), $selection)
+				deletedSolves.pop()
+			}
+		}
+	}
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <div class='flex flex-col text-lg items-center'>
 	{#each [...solves || []].reverse() as solve, i (solve.date.getTime())}
@@ -38,4 +67,6 @@
 	{/each}
 </div>
 
-<!-- <SolveCard solve={showingSolve} editable belongs={selection}/> -->
+<Overlay on:close={ () => showingSolve = null } enabled={ !!showingSolve }>
+	<SolveCard bind:solve={ showingSolve } />
+</Overlay>
