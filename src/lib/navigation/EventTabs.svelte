197e303ba5581add87273dcccf53a3ac28a93cf3
Odilf <odysseas.maheras@gmail.com>
2022-05-21 16:20:40 +0200

    added sessions!

--- src/lib/navigation/EventTabs.svelte
+++ src/lib/navigation/EventTabs.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import { browser } from "$app/env";
 	import type { Event, Session } from "$lib/data/architecture";
+	import EventManager from "$lib/profile/EventManager.svelte";
+	import Overlay from "$lib/UI/basic/Overlay.svelte";
 	import { isOverflown } from "$lib/utils/overflow";
 	import { afterUpdate } from "svelte";
+	import { writable } from "svelte-local-storage-store";
+	import { fly } from "svelte/transition";
 	
 	export let events: Event[]
 	let selected = 0
+
+	const selectedSessions = writable('sessionSelection', {})
+	
+	$: console.log($selectedSessions[editingEvent?.name]);
+	
+	// Initialize if empty
+	if (Object.keys($selectedSessions).length < events.length) {
+		events.forEach(event => {
+			$selectedSessions[event.name] = event.sessions[0]
+		})
+	}
 	
 	export let selection: { event: Event; session: Session; }
-	$: selection = getSelection(selected, 0)
+	$: {
+		const event = events[selected]
+		const session = event.sessions[selectedSessions[event.name]] || event.sessions[0]
+		selection = { event, session }
+	}
 
 	let container: HTMLDivElement
 	$: buttons = browser && container && container.children
@@ -18,12 +37,6 @@
 		inline: "center"
 	})
 
-	function getSelection(selectedEvent: number, selectedSession: number) {
-		const event = events[selectedEvent]
-		const session = event.sessions[selectedSession]
-		return { event, session }
-	}
-
 	function handleKeydown(e: KeyboardEvent) {
 		if (e.altKey || e.shiftKey) {
 			if (e.key === 'ArrowLeft') decrease()
@@ -31,18 +44,25 @@
 		}
 	}
 
-	function decrease() {
-		if (selected > 0) selected -= 1
-	}
-
-	function increase() {
-		if (selected < events.length - 1) selected += 1
-	}
+	const decrease = () => (selected > 0) && (selected -= 1)
+	const increase = () => (selected < events.length - 1) && (selected += 1)
 
 	let overflowing = false
 	$: afterUpdate(() => {
 		overflowing = isOverflown(container)
 	})
+
+	let editingEvent = null
+	function handleClick(index: number) {
+		if (selected !== index) {
+			selected = index
+		} else {
+			editingEvent = events[index]
+		}
+	}
+
+	// For reactivity, apparently
+	$: events[events.indexOf(editingEvent)] = editingEvent
 </script>
 
 <svelte:window on:keydown={handleKeydown} on:resize={() => overflowing = isOverflown(container)}/>
@@ -50,7 +70,7 @@
 <div class='flex items-center w-full'>
 
 {#if overflowing}
-<button class='clickable h-10' on:click={() => decrease()} disabled={container.scrollLeft === 0}>
+<button class='clickable transition h-10 disabled:opacity-50' on:click={() => decrease()} disabled={selected === 0}>
 	<svg xmlns="http://www.w3.org/2000/svg" class="h-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width={4}>
 		<path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
 	</svg>
@@ -59,18 +79,27 @@
 
 <div bind:this={container} class='flex rounded p-2 scrollbar-hide bg-secondary text-primary shadow box-content mx-auto overflow-x-scroll'>
 	{#each events as event, i}
-		<button class='p-2 rounded transition px-4 mx-1 clickable
-		{selected === i ? 'bg-primary text-secondary shadow' : ''}'
-		on:click={() => selected = i}>
+		<button class='p-2 rounded transition px-4 mx-1 hover:scale-105
+		{selected === i ? 'bg-primary text-secondary shadow' : ''} flex flex-col items-center justify-center'
+		on:click={() => handleClick(i)}>
 			<div class='font-bold text-lg'>
 				{event.abbreviation}
 			</div>
+			{#if $selectedSessions[event.name]?.name !== 'Main'}
+				<div class='font-light'>
+					{$selectedSessions[event.name].name}
+				</div>
+			{/if}
 		</button>
 	{/each}	
 </div>
 
+<Overlay enabled={editingEvent}>
+	<EventManager bind:event={editingEvent} bind:selectedSession={$selectedSessions[editingEvent.name]}/>
+</Overlay>
+
 {#if overflowing}
-<button class='clickable h-10' on:click={() => increase()}>
+<button class='clickable transition h-10 disabled:opacity-50' on:click={() => increase()} disabled={selected === events.length - 1}>
 	<svg xmlns="http://www.w3.org/2000/svg" class="h-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width={4}>
 		<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
 	</svg>
--- src/lib/profile/EventManager.svelte
+++ src/lib/profile/EventManager.svelte
@@ -1,42 +1,74 @@
-<!-- <script lang="ts">
-	import { defaultEvents, type Event } from "$lib/data/architecture";
-	import { deleteEvent as deleteEventFromDB, addEvent } from "$lib/data/database";
-	import { user } from ".";
-	
-	export let userEvents: Event[]
+<script lang="ts">
+	import { Session, type Event } from "$lib/data/architecture";
+	import TrashCan from "$lib/UI/icons/TrashCan.svelte";
+	import { getContext } from "svelte";
+	import { fly } from "svelte/transition";
+	import type { Writable } from 'svelte/store'
+	import { flip } from "svelte/animate";
 
-	function isActive(event: Event, events: Event[]) {
-		return events?.some(userEvent => userEvent.name === event.name)
+	export let event: Event
+	export let selectedSession: Session = null
+
+	let newSessionName = ''
+
+	console.log(selectedSession);
+	
+
+	function addSession() {
+		event.sessions = [...event.sessions, new Session(newSessionName)]
+		newSessionName = ''
 	}
 
-	function handleClick(event: Event) {
-		if (isActive(event, userEvents)) {
-			deleteEventFromDB(event, defaultEvents, $user)
-		} else {
-			addEvent(event, defaultEvents, $user)
+	const selection = getContext('selection') as Writable<unknown>
+	console.log('selection from context is', $selection);
+
+	$: valid = event.sessions.every(session => session.name !== newSessionName)
+	
+	function deleteSession(session: Session) {
+		event.sessions = event.sessions.filter(s => s.name !== session.name)
+		if (selectedSession.name === session.name) {
+			selectedSession = event.sessions[0]
 		}
 	}
-
-	// $: console.log(userEvents);
-	
-
 </script>
 
-<div class='flex flex-col items-center'>
-	<span class='text-xl'> Toggle your main events </span>
+<div class='max-w-xl w-screen p-8'>
 
-	{#each defaultEvents as event}
-		<button class='bg-secondary p-2 m-2 rounded text-primary text-lg clickable'
-		class:disabled={!isActive(event, userEvents)}
-		on:click={() => handleClick(event)}>
-			<span class='font-bold'> {event.name} </span>
-			({event.abbreviation})
+	<h1 class='text-5xl font-black'> {event.name} </h1>
+
+	<h2 class='text-3xl mt-5 font-light'> Sessions: </h2> 
+	<div id='separator' class='h-[0.1rem] w-full bg-secondary rounded opacity-50'/>
+
+	<ul class='list-disc'>
+		{#each event.sessions as session (session.name)}
+		<li transition:fly|local={{ y: 10 }}
+		animate:flip>
+		<div class='flex justify-between w-full mt-2
+		{selectedSession.name === session.name && 'bg-secondary p-2 rounded text-primary'}'>
+		<button class='clickable transition' on:click={() => selectedSession = session}>
+			<h2 class='text-2xl font-bold'> {session.name} </h2>
 		</button>
-	{/each}
-</div>
+			
+			{#if session.name !== 'Main'}
+			<button class='clickable transition' on:click={() => deleteSession(session)}> 
+				<TrashCan/>
+			</button>
+			{/if}
+		</div>
+		</li>
+		{/each}
 
-<style lang="postcss">
-	.disabled {
-		@apply bg-black opacity-50;
-	}
-</style> -->
+		<div class='flex flex-col my-4'>
+			<input type='text' class='p-2 rounded shadow' bind:value={newSessionName} placeholder="Enter name for new session" 
+			
+			on:submit={() => addSession()}>
+
+			<button class='clickable bg-secondary text-primary font-bold p-2 rounded shadow my-2 duration-500 transition-all
+			{newSessionName ? '' : 'translate-y-3 opacity-0 pointer-events-none'} disabled:opacity-50'
+			on:click={() => addSession()}
+			disabled={!valid}>
+				Add session
+			</button>
+		</div>
+	</ul>
+</div>
