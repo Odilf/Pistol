95e329923a298f04aa9d6636a3635084d8e6f81b
Odilf <odysseas.maheras@gmail.com>
2022-05-25 14:20:52 +0200

    null safety maybe

--- src/routes/profile/events.svelte
+++ src/routes/profile/events.svelte
@@ -36,12 +36,12 @@
 				{#each $events.filter(event => eventTypes[accessor].includes(event.abbreviation)) as event}
 					<li class='flex items-center'> 
 						<button class='text-xl clickable transition p-2 bg-secondary text-primary rounded shadow my-2 font-bold
-						{event.options.hidden && 'opacity-50'}'
+						{event?.options?.hidden && 'opacity-50'}'
 						on:click={() => toggleVisibility(event)}> 
 							{event.name} 
 						</button> 
 						<button class='clickable transition p-2 ml-2 drop-shadow
-						{event.options.hidden && 'opacity-50'}'
+						{event?.options?.hidden && 'opacity-50'}'
 						on:click={() => edit(event)}> 
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width={2}>
 								<path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
